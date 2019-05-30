4b67d43366a94ff725354f93d01ba37050f2c345
xmastree <huseyinozan.honamlioglu@gunsel.com.tr>
2019-05-30 16:22:19 +0300

    Started to building levels
    * Laying out gemoetry (lights, walls)
    * Moving objects (closing opening door)
    * fix

--- Source/BuildingEscape/PositionReporter.cpp
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -2,16 +2,15 @@
 
 
 #include "PositionReporter.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReporter::UPositionReporter()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -20,7 +19,9 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	FString Objectpos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *Objectpos);
 	
 }
 
